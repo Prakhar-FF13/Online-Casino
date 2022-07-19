23c50db0880413f17e224a20a9eb73e593f61e23
Prakhar Kumar <prakhar.meerut@gmail.com>
2022-07-19 16:36:25 +0530

    Deploy script

---
+++ online-casino-contracts/deploy.js
@@ -0,0 +1,25 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const HDWalletProvider = require("@truffle/hdwallet-provider"),
+  Web3 = require("web3"),
+  contracts = require("./compile"),
+  provider = new HDWalletProvider(process.env.CODE, process.env.LINK),
+  web3 = new Web3(provider),
+  fs = require("fs");
+
+async function deploy() {
+  const fetchedAccounts = await web3.eth.getAccounts();
+  const contractsDeployed = {};
+  for (x in contracts) {
+    result = await new web3.eth.Contract(contracts[x].abi)
+      .deploy({
+        data: "0x" + contracts[x].bytecode,
+      })
+      .send({ from: fetchedAccounts[0], gas: 2000000 });
+
+    contractsDeployed[x] = result;
+  }
+  fs.writeFileSync("contractsDeployed.json", JSON.stringify(contractsDeployed));
+  provider.engine.stop();
+}
+deploy();
--- online-casino-contracts/test.js
+++ online-casino-contracts/test.js
@@ -15,4 +15,18 @@ describe("Deploying the contarct", () => {
 
     assert.ok(result["_address"]);
   });
+
+  describe("Game Functionality", async () => {
+    let contract;
+    beforeEach(async () => {
+      const accounts = await web3.eth.getAccounts();
+      const result = await new web3.eth.Contract(contracts.Roullete.abi)
+        .deploy({
+          data: "0x" + contracts.Roullete.bytecode,
+        })
+        .send({ from: accounts[0], gas: 2000000 });
+
+      contract = result;
+    });
+  });
 });
