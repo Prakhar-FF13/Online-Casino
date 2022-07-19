2d96fcb5dd4f019a38e368a9ced5d7225b9ec09f
Prakhar Kumar <prakhar.meerut@gmail.com>
2022-07-19 18:11:42 +0530

    Basic contract testing

--- online-casino-contracts/contracts/Roullete.sol
+++ online-casino-contracts/contracts/Roullete.sol
@@ -1,26 +1,38 @@
 pragma solidity >=0.8.15;
 
 contract Roullete {
-    mapping(address => Game) ownerToGame;
-    address[] private allGames;
+    enum GameState {
+        NOTCREATED,
+        STARTED,
+        ENDED
+    }
 
     struct Game {
+        address createdBy;
         uint32 numb;
         uint32 prize;
         uint256[] chosen;
         address[] players;
+        GameState state;
     }
 
+    mapping(address => Game) ownerToGame;
+    address[] private allGames;
+
     function createGame(uint32 _numb, uint32 _prize) public {
+        require(ownerToGame[msg.sender].state != GameState.STARTED);
         Game memory g;
+        g.createdBy = msg.sender;
         g.numb = _numb;
         g.prize = _prize;
+        g.state = GameState.STARTED;
         ownerToGame[msg.sender] = g;
         allGames.push(msg.sender);
     }
 
     function joinGame() public payable returns (Game memory) {
         require(allGames.length > 0);
+        require(msg.value >= 0.00001 ether);
         Game storage randGame = ownerToGame[
             allGames[
                 uint256(keccak256(abi.encode(block.timestamp, msg.sender))) %
@@ -34,5 +46,16 @@ contract Roullete {
 
     function fetchGame() public view returns (Game memory) {
         return ownerToGame[msg.sender];
+    }
+
+    function fetchAllGames() public view returns (address[] memory) {
+        return allGames;
+    }
+
+    function endGame() public {
+        Game storage g = ownerToGame[msg.sender];
+        require(g.createdBy == msg.sender);
+        require(g.state != GameState.NOTCREATED && g.state != GameState.ENDED);
+        g.state = GameState.ENDED;
     }
 }
--- online-casino-contracts/test.js
+++ online-casino-contracts/test.js
@@ -4,29 +4,162 @@ const Web3 = require("web3"),
   contracts = require("./compile"),
   assert = require("assert");
 
-describe("Deploying the contarct", () => {
-  it("Should deploy the contract", async () => {
-    const accounts = await web3.eth.getAccounts();
-    const result = await new web3.eth.Contract(contracts.Roullete.abi)
+describe("Roullete Wheel", () => {
+  let accounts, roullete;
+  const gameState = {
+    NOTSTARTED: 0,
+    STARTED: 1,
+    ENDED: 2,
+  };
+
+  beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+    roullete = await new web3.eth.Contract(contracts.Roullete.abi)
       .deploy({
         data: "0x" + contracts.Roullete.bytecode,
       })
       .send({ from: accounts[0], gas: 2000000 });
 
-    assert.ok(result["_address"]);
+    assert.ok(roullete["_address"]);
   });
 
-  describe("Game Functionality", async () => {
-    let contract;
-    beforeEach(async () => {
-      const accounts = await web3.eth.getAccounts();
-      const result = await new web3.eth.Contract(contracts.Roullete.abi)
-        .deploy({
-          data: "0x" + contracts.Roullete.bytecode,
-        })
-        .send({ from: accounts[0], gas: 2000000 });
+  describe("Game Creation", async () => {
+    it("Should create a new game", async () => {
+      await roullete.methods.createGame(10, 3).send({
+        from: accounts[0],
+        gas: 200000,
+      });
 
-      contract = result;
+      const game = await roullete.methods.fetchGame().call();
+      assert.equal(accounts[0], game.createdBy);
+      assert.equal(gameState.STARTED, game.state);
+    });
+
+    it("Should not create a new game if a game is already running", async () => {
+      await roullete.methods.createGame(10, 3).send({
+        from: accounts[0],
+        gas: 200000,
+      });
+
+      try {
+        await roullete.methods.createGame(10, 3).send({
+          from: accounts[0],
+          gas: 200000,
+        });
+
+        throw new Error("Game created twice");
+      } catch (e) {
+        assert.ok(e);
+      }
+    });
+  });
+
+  describe("Game Termination", async () => {
+    it("should end the game if a game is running", async () => {
+      await roullete.methods.createGame(10, 3).send({
+        from: accounts[0],
+        gas: 200000,
+      });
+
+      await roullete.methods.endGame().send({
+        from: accounts[0],
+        gas: 200000,
+      });
+
+      const game = await roullete.methods.fetchGame().call();
+      assert.equal(gameState.ENDED, game.state);
+    });
+
+    it("should not do anything if game has already ended", async () => {
+      await roullete.methods.createGame(10, 3).send({
+        from: accounts[0],
+        gas: 200000,
+      });
+
+      await roullete.methods.endGame().send({
+        from: accounts[0],
+        gas: 200000,
+      });
+
+      try {
+        await roullete.methods.endGame().send({
+          from: accounts[0],
+          gas: 200000,
+        });
+
+        throw new Error("Game ended twice");
+      } catch (e) {
+        assert.ok(e);
+      }
+    });
+
+    it("Should not allow anyone other than game creator to end the game", async () => {
+      await roullete.methods.createGame(10, 3).send({
+        from: accounts[0],
+        gas: 200000,
+      });
+
+      try {
+        await roullete.methods.endGame().send({
+          from: accounts[1],
+          gas: 200000,
+        });
+
+        throw new Error("Game ended by someone other than the owner");
+      } catch (e) {
+        assert.ok(e);
+      }
+    });
+  });
+
+  describe("Game Joining", async () => {
+    it("Should not join if no game is available", async () => {
+      try {
+        await roullete.methods.joinGame().send({
+          from: accounts[0],
+          gas: 200000,
+          value: web3.utils.toWei("0.00002", "ether"),
+        });
+
+        throw new Error("Game joined when no game exists");
+      } catch (e) {
+        assert.ok(e);
+      }
+    });
+
+    it("Should not allow joining the game if minimum bid is not met", async () => {
+      await roullete.methods.createGame(10, 3).send({
+        from: accounts[0],
+        gas: 200000,
+      });
+
+      try {
+        await roullete.methods.joinGame().send({
+          from: accounts[1],
+          gas: 200000,
+          value: web3.utils.toWei("0.000001", "ether"),
+        });
+
+        throw new Error("Game joined when minimum bid not met");
+      } catch (e) {
+        assert.ok(e);
+      }
+    });
+
+    it("Should join the game if a game is available", async () => {
+      await roullete.methods.createGame(10, 3).send({
+        from: accounts[0],
+        gas: 200000,
+      });
+
+      await roullete.methods.joinGame().send({
+        from: accounts[1],
+        gas: 200000,
+        value: web3.utils.toWei("0.00002", "ether"),
+      });
+
+      const game = await roullete.methods.fetchGame().call();
+      assert.equal(accounts[1], game.players[0]);
     });
   });
 });
